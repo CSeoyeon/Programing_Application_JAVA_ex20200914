f6316b485f69a1c292a7a7f8b9f61d8f7f40785c
Seoyeon Choi <choiseoyeon@gmail.com>
2020-09-14 13:09:28 +0900

    Initial sharing of project

---
+++ Rectangle.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Rectangle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Rectangle
+{
+    int width;
+    int height;
+
+    /**
+     * Constructor for objects of class Rectangle
+     */
+    public Rectangle(int width, int height)
+    {
+        this.width = width;
+        this.height = height; 
+    }
+
+}
